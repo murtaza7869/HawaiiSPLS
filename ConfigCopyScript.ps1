9c4e67a61c9931a9fe22c6952b8adebd42f64b21
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-07-13 17:04:14 -0700

    Add files via upload

---
+++ ConfigCopyScript.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest 'https://raw.githubusercontent.com/murtaza7869/HawaiiSPLS/main/Default.JCF' -Outfile 'C:\ProgramData\Freedom Scientific\JAWS\2021\Settings\enu\Default.JCF'
+ping -n 5 127.0.0.1
+Invoke-WebRequest 'https://github.com/murtaza7869/HawaiiSPLS/raw/main/Config.zip' -Outfile 'C:\Windows\Temp\Config.zip'
+ping -n 5 127.0.0.1
+Expand-Archive -LiteralPath C:\Windows\Temp\Config.zip -DestinationPath C:\Users\public.user10\AppData\Roaming\Freedom Scientific\ZoomText\2021\
