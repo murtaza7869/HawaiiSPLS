e01fb222064494e15e54ba22f964a14907410454
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-07-14 09:54:36 -0700

    Update ConfigCopyScript.ps1

--- ConfigCopyScript.ps1
+++ ConfigCopyScript.ps1
@@ -2,4 +2,5 @@ Invoke-WebRequest 'https://raw.githubusercontent.com/murtaza7869/HawaiiSPLS/main
 ping -n 5 127.0.0.1
 Invoke-WebRequest 'https://github.com/murtaza7869/HawaiiSPLS/raw/main/Config.zip' -Outfile 'C:\Windows\Temp\Config.zip'
 ping -n 5 127.0.0.1
+md C:\Users\public.user10\AppData\Roaming\Freedom Scientific\ZoomText\2021
 Expand-Archive -LiteralPath C:\Windows\Temp\Config.zip -DestinationPath C:\Users\public.user10\AppData\Roaming\Freedom Scientific\ZoomText\2021\
