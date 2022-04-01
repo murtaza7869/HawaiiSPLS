d9ea419eb7d7b147ba73353c94528fd44044a6fa
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-04-01 12:58:12 -0700

    Update OutlookPatch.ps1

--- OutlookPatch.ps1
+++ OutlookPatch.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=103499' -Outfile 'C:\Windows\Temp\outlook2016-kb5001998-fullfile-x86-glb.exe'
+Invoke-WebRequest 'https://faronics.digitalpigeon.com/shr/07v1kLH0Eeyp1QYlYT57bQ/ixrVE2z1Yzna_RwT3-nNIA/file/d9882e80-b1f4-11ec-9800-02b7e87b190d/download# - outlook2016-kb5001998-fullfile-x86-glb.exe - outlook2016-kb5001998-fullfile-x86-glb.exe' -Outfile 'C:\Windows\Temp\outlook2016-kb5001998-fullfile-x86-glb.exe'
 ping -n 5 127.0.0.1
 Start-Process -FilePath "C:\Windows\Temp\outlook2016-kb5001998-fullfile-x86-glb.exe" -ArgumentList "/quiet /norestart"
