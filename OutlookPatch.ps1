cc48d3aaa88f1abace34076f1687f6c054dcff8d
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-04-01 12:23:44 -0700

    Create OutlookPatch.ps1

---
+++ OutlookPatch.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=103499' -Outfile 'C:\Windows\Temp\outlook2016-kb5001998-fullfile-x86-glb.exe'
+ping -n 5 127.0.0.1
+Start-Process -FilePath "C:\Windows\Temp\outlook2016-kb5001998-fullfile-x86-glb.exe' -ArgumentList "/quiet /norestart"
